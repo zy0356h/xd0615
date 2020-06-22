d6ad1ee81a67819f37b7a0e849e75e3cb1e74817
zhu yihua <zy0356h@163.com>
2020-06-22 22:13:29 +0800

    这是第七次作业提交，朱翊华19010500033

---
+++ lesson7/code2.c
@@ -0,0 +1,49 @@
+#include <MsTimer2.h>     //定时器库的头文件
+int pinInterrupt = 2; //接中断信号的脚
+int tick = 0; //计数值
+int state=0;
+//中断服务程序
+void myfunc()
+{
+  tick+=1;
+  if(tick==10)
+    tick=0;
+  digitalWrite(8,tick&0x1);
+  digitalWrite(9,(tick>>1)&0x1);
+  digitalWrite(10,(tick>>2)&0x1);
+  digitalWrite(11,(tick>>3)&0x1);
+}
+
+void onChange()
+{
+      tick=-1;
+}
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode( pinInterrupt, INPUT);//设置管脚为输入
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  MsTimer2::set(1000, myfunc); //设置中断，每1000ms进入一次中断服务程序 
+  MsTimer2::start(); //开始计时
+  //Enable中断管脚, 中断服务程序为onChange(), 监视引脚变化
+  attachInterrupt( digitalPinToInterrupt(pinInterrupt), onChange, CHANGE);
+}
+ 
+void loop()
+{
+  
+}
